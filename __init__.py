6057e76bf124ffe9f47f7b1e3640e7a2d51fecf0
Daniel Fuchs <dfuchs@synyx.de>
2018-11-24 01:45:00 +0100

    cmd fart

--- __init__.py
+++ __init__.py
@@ -17,28 +17,7 @@ class MeaningFallback(FallbackSkill):
         # Any other initialize code goes here
 
     def handle_fallback(self, message):
-        """
-            Answers question about the meaning of life, the universe
-            and everything.
-        """
-        utterance = message.data.get("utterance")
-
-        # get keywords for current language
-        what = self.dialog_renderer.render('query')
-        meaning = self.dialog_renderer.render('meaning')
-        life = self.dialog_renderer.render('life')
-        universe = self.dialog_renderer.render('universe')
-        everything = self.dialog_renderer.render('everything')
-
-        if what in utterance \
-            and meaning in utterance \
-            and (life in utterance \
-                or universe in utterance \
-                or everything in utterance):
-            self.speak('42')
-            return True # Indicate that the utterance was handled
-        else:
-            return False
+        print("fart")
 
     def shutdown(self):
         """
