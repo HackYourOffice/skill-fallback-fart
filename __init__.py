56cab12cb696aca9f5970c453ebb6fd63f2967a6
Daniel Fuchs <dfuchs@synyx.de>
2018-11-24 03:06:05 +0100

    fart direct

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ class MeaningFallback(FallbackSkill):
         #api_url = "http://furby-control.synyx.coffee:3872/cmd/action"
         #requests.post(api_url, data='{"params":{"input":7,"index":2,"subindex":0,"specific":0}}')
         time.sleep(1)
-        fart = "./fart-0%d.mp3" % (random.randint(1, 8))
+        fart = "/opt/mycroft/skills/skill-fallback-fart.hackyouroffice/fart-0%d.mp3" % (random.randint(1, 8))
         mixer.init()
         mixer.music.load(fart)
         mixer.music.play()
